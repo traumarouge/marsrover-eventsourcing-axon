7f1e2d2bdf8a4b608b3094c9b9e22c820aa6e1b0
traumarouge <traumarouge@gmail.com>
2019-09-08 12:18:50 +0200

    Configure EventStorageEngine
    In order to use event sourcing an EventStorageEngine must be
    configured, in this case one that stores events in memory.
    
    If the axon-server-connector module is excluded the
    EmbeddedEventStore will be auto-configured, given a concrete
    implementation of EventStorageEngine is available.

--- src/main/java/marsrover/MarsRoverApplication.java
+++ src/main/java/marsrover/MarsRoverApplication.java
@@ -1,7 +1,12 @@
 package marsrover;
 
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -10,5 +15,12 @@ public class MarsRoverApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(MarsRoverApplication.class, args);
+    }
+
+
+    @Bean
+    public EventStorageEngine storageEngine() {
+
+        return new InMemoryEventStorageEngine();
     }
 }
